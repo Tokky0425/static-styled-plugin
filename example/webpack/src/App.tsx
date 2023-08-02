0d9e33319523ecd05226c3336b190a789934b6b8
Tokky0425 <lily.yarn.5@gmail.com>
2023-08-03 00:35:48 +0900

    add same component as vite example to webpack example

--- example/webpack/src/App.tsx
+++ example/webpack/src/App.tsx
@@ -1,10 +1,47 @@
-import { ReactNode } from 'react'
+import { useState, ReactNode } from 'react'
 import styled from 'styled-components'
+import { Button } from './Button'
 
-export function App(): ReactNode {
-  return <Title>Hello</Title>
+function App() {
+  const [showText, setShowText] = useState(true)
+  return (
+    <div>
+      <Button onClick={() => setShowText((prev) => !prev)}>toggle color</Button>
+      <Title color="red">Blue</Title>
+      <OrangeText>orange</OrangeText>
+      {showText && <Text>red</Text>}
+      <BlackText>black</BlackText>
+      {showText && <PinkText>pink</PinkText>}
+      <PinkText>pink</PinkText>
+    </div>
+  )
 }
 
-const Title = styled.h1`
+
+function TextComponent (props: { color: "red" | "blue", children: ReactNode, className?: string }) {
+  return <h1 color={props.color} className={props.className}>{props.children}</h1>
+}
+
+const Title = styled(TextComponent)`
+  font-size: 54px;
+  color: blue;
+`
+
+const Text = styled.p`
+  display: flex;
   color: red;
 `
+
+const BlackText = styled(Text)`
+  color: black;
+`
+
+const OrangeText = styled(BlackText)`
+  color: orange;
+`
+
+const PinkText = styled(Text)`
+  color: pink;
+`
+
+export default App
