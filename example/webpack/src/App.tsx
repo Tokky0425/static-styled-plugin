152fa67e89aabdf22ceb1eef69bc3f21a67b5660
Tokky0425 <lily.yarn.5@gmail.com>
2023-08-13 22:45:14 +0900

    simplify examples

--- example/webpack/src/App.tsx
+++ example/webpack/src/App.tsx
@@ -1,49 +1,22 @@
-import { useState, ReactNode } from 'react'
 import styled, { ThemeProvider } from 'styled-components'
-import { Button } from './Button'
 import { theme } from './theme'
 
 function App() {
-  const [showText, setShowText] = useState(true)
   return (
     <ThemeProvider theme={theme}>
-      <Button onClick={() => setShowText((prev) => !prev)}>toggle color</Button>
-      <Title color="red">Blue</Title>
-      <OrangeText>orange</OrangeText>
-      {showText && <Text>red</Text>}
-      <BlackText>black</BlackText>
-      {showText && <PinkText>pink</PinkText>}
-      <PinkText>pink</PinkText>
+      <StaticStyleText>static style text</StaticStyleText>
+      <DynamicStyleText>dynamic style text</DynamicStyleText>
     </ThemeProvider>
   )
 }
 
-
-function TextComponent (props: { color: "red" | "blue", children: ReactNode, className?: string }) {
-  return <h1 color={props.color} className={props.className}>{props.children}</h1>
-}
-
-const Title = styled(TextComponent)`
-  font-size: 54px;
-  color: blue;
-`
-
-const Text = styled.p`
-  display: flex;
-  color: red;
+const StaticStyleText = styled.p`
+  color: coral;
   font-size: ${(props) => props.theme.fontSize.l};
 `
 
-const BlackText = styled(Text)`
-  color: black;
-`
-
-const OrangeText = styled(BlackText)`
-  color: orange;
-`
-
-const PinkText = styled(Text)`
-  color: pink;
+const DynamicStyleText = styled(StaticStyleText)`
+  color: navy;
 `
 
 export default App
