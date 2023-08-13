dd0c0704c7f64738f0680931eb50b33ef0c09ff0
Tokky0425 <lily.yarn.5@gmail.com>
2023-08-13 23:45:24 +0900

    add getStyledFunctionName

---
+++ packages/compiler/src/getStyledFunctionName.ts
@@ -0,0 +1,16 @@
+import { Node, SourceFile } from 'ts-morph'
+
+export function getStyledFunctionName(file: SourceFile) {
+  let styledFunctionName: string | null = null
+
+  file.forEachDescendant((node) => {
+    if (styledFunctionName) return
+    if (!Node.isImportDeclaration(node)) return
+    if (node.getModuleSpecifier().getText() === '\'styled-components\'') {
+      const importClause = node.getImportClause()
+      styledFunctionName = importClause?.getDefaultImport()?.getText() ?? null
+    }
+  })
+
+  return styledFunctionName
+}
--- packages/compiler/src/index.ts
+++ packages/compiler/src/index.ts
@@ -2,15 +2,19 @@ import { processTaggedTemplateExpression } from './processTaggedTemplateExpressi
 import { Theme } from './types'
 import { Node, Project } from 'ts-morph'
 export { parseTheme } from './parseTheme'
+import { getStyledFunctionName } from './getStyledFunctionName'
 export type { Theme } from './types'
 
 const project = new Project()
 
 export function compile(code: string, filePath: string, theme: Theme | null) {
   const file = project.createSourceFile(filePath, code, { overwrite: true })
+  const styledFunctionName = getStyledFunctionName(file)
+  if (!styledFunctionName) return file.getFullText()
+
   file.forEachDescendant((node) => {
     if (Node.isTaggedTemplateExpression(node)) {
-      processTaggedTemplateExpression(node, 'styled', theme)
+      processTaggedTemplateExpression(node, styledFunctionName, theme)
     }
   })
   return file.getFullText()
