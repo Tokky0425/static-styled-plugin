857b679594b466485ee59ae5381b397be1c77094
Tokky0425 <lily.yarn.5@gmail.com>
2023-11-04 15:34:36 +0900

    theme must be declared with `as const`

--- packages/compiler/src/parseTheme.ts
+++ packages/compiler/src/parseTheme.ts
@@ -5,6 +5,14 @@ import type { Theme } from './types'
 import { Evaluator, TsEvalError } from './Evaluator'
 
 const project = new Project()
+
+/**
+ * theme must meet the following conditions
+ * - it is declared with the name `theme`
+ * - it is an object literal
+ * - it is declared with const assertion (`as const`)
+ * - it does not depend on values from node_modules
+ */
 export function parseTheme(themeFileRelativePath: string): null | Theme {
   let themeResult: null | Theme = null
   const themeFilePath = path.join(process.cwd(), themeFileRelativePath)
@@ -17,8 +25,12 @@ export function parseTheme(themeFileRelativePath: string): null | Theme {
   for (const variableDeclaration of variableDeclarations) {
     if (themeResult) continue
     if (variableDeclaration.getName() === 'theme') {
-      const value = variableDeclaration.getInitializer()
+      const initializer = variableDeclaration.getInitializer()
+      if (!Node.isAsExpression(initializer)) continue
+
+      const value = initializer.getExpression()
       if (!value || !Node.isObjectLiteralExpression(value)) continue
+
       const evaluator = new Evaluator({ extra: {}, definition: { cssFunctionName: null }, theme: null })
       const objectLiteralResult = evaluator.evaluateObjectLiteralExpression(value)
       if (objectLiteralResult !== TsEvalError) {
