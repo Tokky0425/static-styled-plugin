2b1f66eecfe7eb8f38cf20ba4fe09e82b788ceb9
Tokky0425 <lily.yarn.5@gmail.com>
2023-11-05 11:28:00 +0900

    add prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,11 @@
-/* eslint-env node */
+/** @type {import('eslint/lib/shared/types').ConfigData} */
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   root: true,
   parserOptions: {
     tsconfigRootDir: __dirname,
     project: ['./tsconfig.json', './packages/*/tsconfig.json'],
-  },
-  rules: {
-    semi: ['error', 'never'],
-    quotes: ['error', 'single']
   },
 }
