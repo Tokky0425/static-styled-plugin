0a9c1f14277da54c4e43fa7581bacd1e6b417c92
Tokky0425 <lily.yarn.5@gmail.com>
2024-02-16 23:24:45 +0900

    add styled-components

--- example/next-pages-dir/pages/_document.tsx
+++ example/next-pages-dir/pages/_document.tsx
@@ -1,13 +1,29 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+// https://github.com/vercel/next.js/blob/96599a1b1d48860e9a5646c66d137621615d37e9/examples/with-styled-components/pages/_document.tsx
+import type { DocumentContext, DocumentInitialProps } from 'next/document'
+import Document from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+export default class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext,
+  ): Promise<DocumentInitialProps> {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: [initialProps.styles, sheet.getStyleElement()],
+      }
+    } finally {
+      sheet.seal()
+    }
+  }
 }
--- example/next-pages-dir/pages/index.tsx
+++ example/next-pages-dir/pages/index.tsx
@@ -1,3 +1,24 @@
+import styled from 'styled-components'
+
+import { ThemeProvider } from 'styled-components'
+import { BoldText } from '@/components/BoldText'
+import { theme } from '@/theme/theme'
+
 export default function Home() {
-  return <h1>Hello, world!</h1>
+  return (
+    <ThemeProvider theme={theme}>
+      <StaticStyleText>static style text</StaticStyleText>
+      <DynamicStyleText>dynamic style text</DynamicStyleText>
+      <BoldText>bold text</BoldText>
+    </ThemeProvider>
+  )
 }
+
+const StaticStyleText = styled.p`
+  color: coral;
+  font-size: ${(props) => props.theme.fontSize.l};
+`
+
+const DynamicStyleText = styled(StaticStyleText)`
+  color: navy;
+`
