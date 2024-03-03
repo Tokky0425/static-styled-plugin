a10888efb17c3ea30e6709d055091c81d3c318b9
Tokky0425 <lily.yarn.5@gmail.com>
2024-03-03 18:49:47 +0900

    now eslint works on vite.config.ts

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,6 +6,6 @@ module.exports = {
   root: true,
   parserOptions: {
     tsconfigRootDir: __dirname,
-    project: ['./tsconfig.json', './packages/*/tsconfig.json'],
+    project: ['./tsconfig.json', './tsconfig.eslint.json', './packages/**/tsconfig.json'],
   },
 }
